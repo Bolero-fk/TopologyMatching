a30577b5f6ebf57f10a2cf41561786fb22cc1d24
FukudaKento <fuku_0000@outlook.jp>
2023-07-27 13:28:01 +0900

    jsonが存在する場合はデータを追加する処理を書いた

--- TopologyCardRegistrar/TopologyCardRegistrar/JsonSaver.cs
+++ TopologyCardRegistrar/TopologyCardRegistrar/JsonSaver.cs
@@ -7,15 +7,35 @@ public class JsonSaver
     {
     }
 
+    class TopologyCard
+    {
+        public string ImageName { get; set; }
+        public int[] HoleCount { get; set; }
+    }
+
+
     static public void SaveJson(string jsonPath, string imgName, int[] holeCounts)
     {
-        var dataToWrite = new
+        List<TopologyCard> topologyCards = new List<TopologyCard>();
+        if (File.Exists(jsonPath))
+        {
+            string existingJson = File.ReadAllText(jsonPath);
+            List<TopologyCard>? readData = JsonConvert.DeserializeObject<List<TopologyCard>>(existingJson);
+
+            if (readData != null)
+                topologyCards = readData;
+        }
+
+        topologyCards.RemoveAll(x => x.ImageName == imgName);
+
+        var topologyCard = new TopologyCard
         {
             ImageName = imgName,
             HoleCount = holeCounts,
         };
+        topologyCards.Add(topologyCard);
 
-        string jsonOutput = JsonConvert.SerializeObject(dataToWrite);
+        string jsonOutput = JsonConvert.SerializeObject(topologyCards);
 
         File.WriteAllText(jsonPath, jsonOutput);
     }
