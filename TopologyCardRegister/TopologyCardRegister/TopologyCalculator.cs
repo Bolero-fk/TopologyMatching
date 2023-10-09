7ed4c14ca319be718e4f917ec528bdd4457846e5
FukudaKento <fuku_0000@outlook.jp>
2023-10-09 18:15:05 +0900

    Posクラスを作成した

--- TopologyCardRegister/TopologyCardRegister/TopologyCalculator.cs
+++ TopologyCardRegister/TopologyCardRegister/TopologyCalculator.cs
@@ -104,42 +104,6 @@
             }
         }
 
-        /// <summary>
-        /// グリッドの座標を管理するクラスです
-        /// </summary>
-        public class Pos
-        {
-            public int X { get; }
-            public int Y { get; }
-
-            public Pos()
-            {
-                this.X = 0;
-                this.Y = 0;
-            }
-
-            public Pos(int x, int y)
-            {
-                this.X = x;
-                this.Y = y;
-            }
-
-            public static Pos operator +(Pos a, Pos b)
-            {
-                return new Pos(a.X + b.X, a.Y + b.Y);
-            }
-
-            public static Pos operator -(Pos a, Pos b)
-            {
-                return new Pos(a.X - b.X, a.Y - b.Y);
-            }
-
-            public static readonly Pos UP = new Pos(0, 1);
-            public static readonly Pos RIGHT = new Pos(1, 0);
-            public static readonly Pos DOWN = new Pos(0, -1);
-            public static readonly Pos LEFT = new Pos(-1, 0);
-        }
-
         /*
          * 以下の図の「.」を白、「#」を黒としたときに、黒のパーツの数が1、その穴の数が1となるように
          * 黒の隣接判定は8方向、白の隣接判定は4方向にする
