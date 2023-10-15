1b64a2b23d3aef4dc9ba39bfcb3803de4ca0a048
FukudaKento <fuku_0000@outlook.jp>
2023-10-15 17:28:52 +0900

    各関数と変数にコメントを追加した

--- TopologyCardRegister/TopologyCardRegister/TopologyCard.cs
+++ TopologyCardRegister/TopologyCardRegister/TopologyCard.cs
@@ -2,14 +2,27 @@ namespace TopologyCardRegister
 {
     public class TopologyCard
     {
+        /// <summary> 表示する画像の縦幅 </summary>
         private const int DISPLAY_IMAGE_HEIGHT_IN_PIXELS = 1024;
+
+        /// <summary> 表示する画像の横幅 </summary>
         private const int DISPLAY_IMAGE_WIDTH_IN_PIXELS = 1024;
+
+        /// <summary> 表示する画像の背景色 </summary>
         private static readonly Color DISPLAY_IMAGE_BACKGROUND_COLOR = Color.White;
 
+        /// <summary> 入力した画像のファイルパス </summary>
         public string SvgFilePath { get; }
+
+        /// <summary> 入力した画像のbitmapデータ </summary>
         public Bitmap SvgImage { get; }
+
+        /// <summary> 入力した画像の図形の穴の数 </summary>
         public int[] HoleCounts { get; }
 
+        /// <summary>
+        /// デフォルトコンストラクター。各プロパティを初期状態に設定します。
+        /// </summary>
         public TopologyCard()
         {
             this.SvgFilePath = string.Empty;
@@ -17,6 +30,10 @@ namespace TopologyCardRegister
             this.HoleCounts = Array.Empty<int>();
         }
 
+        /// <summary>
+        /// コンストラクター。指定されたSVGファイルパスから画像を読み込み、図形の穴の数を計算します。
+        /// </summary>
+        /// <param name="svgFilePath">SVGファイルのパス</param>
         public TopologyCard(string svgFilePath)
         {
             this.SvgFilePath = svgFilePath;
@@ -24,6 +41,10 @@ namespace TopologyCardRegister
             this.HoleCounts = TopologyStatusCalculator.CalculateHoleCount(this.SvgImage).ToArray();
         }
 
+        /// <summary>
+        /// 指定されたSVGファイルパスからSVGドキュメントを読み込み、背景色を設定してBitmapとして返します。
+        /// </summary>
+        /// <returns>読み込んだSVG画像のBitmapデータ</returns>
         private Bitmap LoadSvg()
         {
             var svgDocument = Svg.SvgDocument.Open(this.SvgFilePath);
@@ -40,6 +61,11 @@ namespace TopologyCardRegister
             return svgDocument.Draw();
         }
 
+        /// <summary>
+        /// このオブジェクトのSVG画像とJSONデータを指定されたパスに保存します。
+        /// </summary>
+        /// <param name="svgFolderPath">SVG画像を保存するフォルダのパス</param>
+        /// <param name="jsonPath">JSONデータを保存するファイルのパス</param>
         public void Save(string svgFolderPath, string jsonPath)
         {
             var svgFileName = Path.GetFileName(this.SvgFilePath);
