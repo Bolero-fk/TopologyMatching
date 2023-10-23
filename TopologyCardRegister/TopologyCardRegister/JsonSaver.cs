ac8adebf628c94abdb32df2da064c1682bb30db9
FukudaKento <fuku_0000@outlook.jp>
2023-10-23 22:51:33 +0900

    JsonSaverクラスにコメントを追記した

--- TopologyCardRegister/TopologyCardRegister/JsonSaver.cs
+++ TopologyCardRegister/TopologyCardRegister/JsonSaver.cs
@@ -1,25 +1,38 @@
-﻿namespace TopologyCardRegister
+namespace TopologyCardRegister
 {
     using Newtonsoft.Json;
 
     public class JsonSaver
     {
+        /// <summary>
+        /// Json出力用のトポロジカードの情報を保持するクラス。
+        /// </summary>
         private class TopologyCard
         {
+            /// <summary> json先に保存する画像の名前。 </summary>
             public string ImageName { get; set; }
+            /// <summary> 保存する画像のホールの数。 </summary>
             public int[] HoleCount { get; set; }
 
-            public TopologyCard(string imageNamem, int[] holeCount)
+            /// <summary>
+            /// Json出力用のトポロジカードの新しいインスタンスを初期化します。
+            /// </summary>
+            /// <param name="imageName">画像の名前。</param>
+            /// <param name="holeCount">ホールの数。</param>
+            public TopologyCard(string imageName, int[] holeCount)
             {
-                this.ImageName = imageNamem;
+                this.ImageName = imageName;
                 this.HoleCount = holeCount;
             }
         }
 
-
         /// <summary>
-        /// 入力されたholeCountと画像名をjsonに保存します
+        /// 入力されたholeCountと画像名をjsonに保存します。
+        /// 既存の同名の画像が存在する場合、holeCountを上書きします。
         /// </summary>
+        /// <param name="jsonPath">保存先のjsonファイルのパス。</param>
+        /// <param name="imageName">保存する画像の名前。</param>
+        /// <param name="holeCount">保存するホールの数。</param>
         public static void SaveJson(string jsonPath, string imageName, int[] holeCount)
         {
             var topologyCards = new List<TopologyCard>();
@@ -37,8 +50,12 @@
         }
 
         /// <summary>
-        /// 指定されたjsonが既に存在する場合は内容を読み込みます。
+        /// 指定されたJSONファイルのパスからトポロジーカードのリストをロードします。
+        /// ファイルが存在し、正しくデシリアライズできる場合はその内容を返します。
+        /// それ以外の場合は空のリストを返します。
         /// </summary>
+        /// <param name="jsonPath">トポロジーカードのリストを含むJSONファイルのパス。</param>
+        /// <returns>JSONファイルからロードしたトポロジーカードのリスト。ファイルが存在しない、または読み込みに失敗した場合は空のリスト。</returns>
         private static List<TopologyCard> LoadTopologyCardJson(string jsonPath)
         {
             try
