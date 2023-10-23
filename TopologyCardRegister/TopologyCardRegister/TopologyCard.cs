22a5c525c9805e8162fa9cb74528fddcbd02c7d6
FukudaKento <fuku_0000@outlook.jp>
2023-10-23 23:14:54 +0900

    TopologyCardクラスにコメントを追記した

--- TopologyCardRegister/TopologyCardRegister/TopologyCard.cs
+++ TopologyCardRegister/TopologyCardRegister/TopologyCard.cs
@@ -1,7 +1,13 @@
 namespace TopologyCardRegister
 {
+    /// <summary>
+    /// トポロジーカードを管理し、SVG画像と関連データの読み書きをサポートするクラス。
+    /// </summary>
     public class TopologyCard
     {
+        /// <summary>
+        /// SVGファイルパスが設定されていない場合にスローされる例外。
+        /// </summary>
         public class NotSetSvgFilePathException : Exception
         {
             public NotSetSvgFilePathException(string message) : base(message)
@@ -9,22 +15,22 @@ namespace TopologyCardRegister
             }
         }
 
-        /// <summary> 表示する画像の縦幅 </summary>
+        /// <summary> 表示する画像の縦幅。 </summary>
         private const int DISPLAY_IMAGE_HEIGHT_IN_PIXELS = 1024;
 
-        /// <summary> 表示する画像の横幅 </summary>
+        /// <summary> 表示する画像の横幅。 </summary>
         private const int DISPLAY_IMAGE_WIDTH_IN_PIXELS = 1024;
 
-        /// <summary> 表示する画像の背景色 </summary>
+        /// <summary> 表示する画像の背景色。 </summary>
         private static readonly Color DISPLAY_IMAGE_BACKGROUND_COLOR = Color.White;
 
-        /// <summary> 入力した画像のファイルパス </summary>
+        /// <summary> 入力した画像のファイルパス。 </summary>
         public string SvgFilePath { get; }
 
-        /// <summary> 入力した画像のbitmapデータ </summary>
+        /// <summary> 入力した画像のbitmapデータ。 </summary>
         public Bitmap SvgImage { get; }
 
-        /// <summary> 入力した画像の図形の穴の数 </summary>
+        /// <summary> 入力した画像の図形の穴の数。 </summary>
         public int[] HoleCounts { get; }
 
         /// <summary>
@@ -51,7 +57,7 @@ namespace TopologyCardRegister
         /// <summary>
         /// 指定されたSVGファイルパスからSVGドキュメントを読み込み、背景色を設定してBitmapとして返します。
         /// </summary>
-        /// <returns>読み込んだSVG画像のBitmapデータ</returns>
+        /// <returns>読み込んだSVG画像のBitmapデータ。</returns>
         private Bitmap LoadSvg()
         {
             var svgDocument = Svg.SvgDocument.Open(this.SvgFilePath);
@@ -71,8 +77,8 @@ namespace TopologyCardRegister
         /// <summary>
         /// このオブジェクトのSVG画像とJSONデータを指定されたパスに保存します。
         /// </summary>
-        /// <param name="svgFolderPath">SVG画像を保存するフォルダのパス</param>
-        /// <param name="jsonPath">JSONデータを保存するファイルのパス</param>
+        /// <param name="svgFolderPath">SVG画像を保存するフォルダのパス。</param>
+        /// <param name="jsonPath">JSONデータを保存するファイルのパス。</param>
         public void Save(string svgFolderPath, string jsonPath)
         {
             if (this.SvgFilePath == string.Empty)
