2c8f4292f4e996f54f6997507f0547e6e4501399
FukudaKento <fuku_0000@outlook.jp>
2023-10-25 23:19:06 +0900

    TopologyStatusCalculatorクラスにコメントを追記した

--- TopologyCardRegister/TopologyCardRegister/TopologyCalculator.cs
+++ TopologyCardRegister/TopologyCardRegister/TopologyCalculator.cs
@@ -1,7 +1,11 @@
 namespace TopologyCardRegister
 {
+    /// <summary>
+    /// 入力されたbitmapの穴の数を計算するクラスです。
+    /// </summary>
     public class TopologyStatusCalculator
     {
+        /// <summary> 入力された画像の白黒を判別する際の輝度の閾値 </summary>
         private const float BRIGHTNESS_THRESHOLD = 0.5f;
 
         /// <summary> 黒画素が画像の端にあると正しく穴の判定ができないので入力画像の余白を設定する </summary>
@@ -22,13 +26,14 @@ namespace TopologyCardRegister
         private static readonly Pos[] WHITE_NEXT_DIRECTIONS = new Pos[] { Pos.UP, Pos.RIGHT, Pos.DOWN, Pos.LEFT };
 
         /// <summary>
-        /// 入力された図形の各連結成分の穴の数を数えて昇順にして返します
+        /// 入力された図形の各連結成分の穴の数を数えて昇順にして返します。
         /// </summary>
+        /// <param name="bitmap">入力される図形を表すビットマップ。</param>
+        /// <returns>各連結成分の穴の数のリストを昇順で返します。</returns>
         public static List<int> Execute(Bitmap bitmap)
         {
             CheckBitmapNotNull(bitmap);
 
-            // 入力された画像を二値化したグラフに変換します
             var grid = BitmapToMonochromeGridConverter.Execute(bitmap, INPUT_IMAGE_PADDING_SIZE, BRIGHTNESS_THRESHOLD);
 
             ChangeNoiseCellColor(grid);
@@ -39,6 +44,12 @@ namespace TopologyCardRegister
 
             return holeCount;
         }
+
+        /// <summary>
+        /// 指定されたグリッドに基づいて各黒色成分の隣接する白色成分の数から穴の数を計算し、その結果を昇順に並べたリストとして返します。
+        /// </summary>
+        /// <param name="grid">MonochromeCellを要素とするグリッド。</param>
+        /// <returns>各黒色成分に隣接する白色成分の数から算出した穴の数のリスト。このリストは昇順にソートされています。</returns>
 
         private static List<int> CalculateHoleCounts(Grid<MonochromeCell> grid)
         {
@@ -59,6 +70,12 @@ namespace TopologyCardRegister
             return holeCount;
         }
 
+        /// <summary>
+        /// 与えられたBitmapオブジェクトがnullでないことを確認します。nullの場合、ArgumentNullExceptionをスローします。
+        /// </summary>
+        /// <param name="bitmap">確認対象のBitmapオブジェクト。</param>
+        /// <exception cref="ArgumentNullException">Bitmapオブジェクトがnullの場合にスローされます。</exception>
+
         private static void CheckBitmapNotNull(Bitmap bitmap)
         {
             if (bitmap == null)
@@ -68,12 +85,14 @@ namespace TopologyCardRegister
         }
 
         /// <summary>
-        /// 隣接するセルの色が全て別の色になっているようなセルはノイズとみなして色を変えます
-        /// 以下の図の#はノイズと認識されます
+        /// グリッド内の各セルを調査して、隣接するセルの色が全て異なるセル（ノイズとして認識されるセル）を見つけ、その色を反転します。
+        /// 例えば、ノイズとして認識されるセルは以下の図の#のようなものです。
         /// ...
         /// .#.
         /// ...
         /// </summary>
+        /// <param name="grid">MonochromeCellのグリッド。このグリッド内のセルがノイズであるかどうかを判定し、必要に応じて色を反転させます。</param>
+
         private static void ChangeNoiseCellColor(Grid<MonochromeCell> grid)
         {
             foreach (var noiseCell in FindNoiseCell(grid))
@@ -81,6 +100,13 @@ namespace TopologyCardRegister
                 noiseCell.InvertColor();
             }
         }
+
+        /// <summary>
+        /// グリッド内のノイズとして認識されるセルを探索し、それらのセルのリストを返します。
+        /// ノイズセルは、その周囲のすべてのセルが異なる色であるセルとして定義されます。
+        /// </summary>
+        /// <param name="grid">MonochromeCellのグリッド。このグリッド内のセルがノイズであるかどうかを判定します。</param>
+        /// <returns>ノイズとして認識されるセルのリストを返します。</returns>
 
         private static List<MonochromeCell> FindNoiseCell(Grid<MonochromeCell> grid)
         {
@@ -99,8 +125,13 @@ namespace TopologyCardRegister
         }
 
         /// <summary>
-        /// posの位置にあるセルがノイズかどうかを判定します
+        /// 指定された位置のセルがノイズであるかを判定します。
+        /// ノイズセルは、その周囲のすべてのセルが異なる色であるセルとして定義されます。
         /// </summary>
+        /// <param name="pos">ノイズであるかどうかを判定したいセルの位置。</param>
+        /// <param name="grid">MonochromeCellのグリッド。このグリッド内のセルがノイズであるかどうかを判定します。</param>
+        /// <returns>指定された位置のセルがノイズであればtrue、それ以外の場合はfalseを返します。</returns>
+
         private static bool IsNoise(Pos pos, Grid<MonochromeCell> grid)
         {
             foreach (var nextPos in FindNextPos(pos, grid))
@@ -123,8 +154,10 @@ namespace TopologyCardRegister
         }
 
         /// <summary>
-        /// セグメントIdをグリッドのセルに割り当てます
+        /// グリッド内のすべてのセルにセグメントIDを割り当てます。
+        /// 未割り当てのセルが見つかった場合、そのセルと同じ色で連結しているすべてのセルに新しいセグメントIDを割り当てます。
         /// </summary>
+        /// <param name="grid">MonochromeCellのグリッド。このグリッド内のセルにセグメントIDを割り当てます。</param>
         private static void AssignSegmentIdToGridCell(Grid<MonochromeCell> grid)
         {
             var segmentCount = 0;
@@ -145,8 +178,11 @@ namespace TopologyCardRegister
         }
 
         /// <summary>
-        /// startPosの位置にあるセルと連結しているセグメントにidを割り当てます
+        /// 指定された位置のセルと連結しているすべてのセルに、指定されたIDを割り当てます。
         /// </summary>
+        /// <param name="startPos">セグメントの基準となる位置を指定するセル。</param>
+        /// <param name="id">連結しているセグメントに割り当てるID。</param>
+        /// <param name="grid">MonochromeCellのグリッド。このグリッド内のセルにセグメントIDを割り当てます。</param>
         private static void AssignSegmentIdToSameSegmentCell(Pos startPos, int id, Grid<MonochromeCell> grid)
         {
             foreach (var pos in FindEqualSegmentPos(startPos, grid))
@@ -154,6 +190,13 @@ namespace TopologyCardRegister
                 grid[pos].SegmentId = id;
             }
         }
+
+        /// <summary>
+        /// 指定された位置のセルと同じ色を持つ、連結しているすべてのセルの位置を探索し、HashSetとして返します。
+        /// </summary>
+        /// <param name="pos">探索を開始するセルの位置。</param>
+        /// <param name="grid">MonochromeCellのグリッド。</param>
+        /// <returns>指定された位置のセルと同じ色を持ち、連結しているすべてのセルの位置のHashSet。</returns>
 
         private static HashSet<Pos> FindEqualSegmentPos(Pos pos, Grid<MonochromeCell> grid)
         {
@@ -186,9 +229,12 @@ namespace TopologyCardRegister
         }
 
         /// <summary>
-        /// 各黒色成分の隣にある白成分を返します
+        /// 黒色成分と隣接する白色成分のセグメントIdを計算します。
         /// </summary>
-        /// <returns>result[黒色成分のセグメントId]: キーに使われているセグメントに隣接する白色セグメントのId</returns>
+        /// <param name="grid">MonochromeCellのグリッド。</param>
+        /// <returns>
+        /// Dictionaryのキーは黒色成分のセグメントId、値はそのキーのセグメントに隣接する白色セグメントのIdのHashSet。
+        /// </returns>
         private static Dictionary<int, HashSet<int>> CalculateWhiteSegmentIdNextToBlackSegment(Grid<MonochromeCell> grid)
         {
             var whiteSegmentIds = FindBlackSegmentIds(grid).ToDictionary(blackSegmentId => blackSegmentId, _ => new HashSet<int>());
@@ -212,6 +258,11 @@ namespace TopologyCardRegister
             return whiteSegmentIds;
         }
 
+        /// <summary>
+        /// グリッド内のすべての黒色セルの位置をリストとして返します。
+        /// </summary>
+        /// <param name="grid">MonochromeCellのグリッド。</param>
+        /// <returns>グリッド内の黒色セルの位置を含むリスト。</returns>
         private static List<Pos> FindBlackPositions(Grid<MonochromeCell> grid)
         {
             var blackPositions = new List<Pos>();
@@ -228,6 +279,12 @@ namespace TopologyCardRegister
             return blackPositions;
         }
 
+        /// <summary>
+        /// グリッド内のすべての黒色セグメントのIDをリストとして返します。
+        /// </summary>
+        /// <param name="grid">MonochromeCellのグリッド。</param>
+        /// <returns>グリッド内の黒色セグメントのIDを含むリスト。</returns>
+
         private static List<int> FindBlackSegmentIds(Grid<MonochromeCell> grid)
         {
             var result = new HashSet<int>();
@@ -243,6 +300,12 @@ namespace TopologyCardRegister
             return result.ToList();
         }
 
+        /// <summary>
+        /// 指定された位置から次に移動可能な位置を検索します。
+        /// </summary>
+        /// <param name="pos">現在の位置</param>
+        /// <param name="grid">MonochromeCellのグリッド。</param>
+        /// <returns>指定された位置から次に移動可能な位置のリスト。</returns>
         private static List<Pos> FindNextPos(Pos pos, Grid<MonochromeCell> grid)
         {
             var result = new List<Pos>();
