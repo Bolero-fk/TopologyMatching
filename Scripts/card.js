2d815edd9ff2659d9c380d57b316ac0345f6dfb9
Bolero-fk <fuku_0000@outlook.jp>
2023-10-31 22:37:43 +0900

    全てのtypescriptコードをコンパイルしなおした

--- Scripts/card.js
+++ Scripts/card.js
@@ -1,18 +1,15 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Card = exports.FlipStatus = void 0;
 /**
  * カードの表面または裏面を示す列挙型
  */
-var FlipStatus;
+export var FlipStatus;
 (function (FlipStatus) {
     FlipStatus[FlipStatus["Front"] = 0] = "Front";
     FlipStatus[FlipStatus["Back"] = 1] = "Back";
-})(FlipStatus || (exports.FlipStatus = FlipStatus = {}));
+})(FlipStatus || (FlipStatus = {}));
 /**
  * カードの振る舞いと状態を管理するクラス
  */
-var Card = /** @class */ (function () {
+export class Card {
     /**
      * Cardクラスのコンストラクタ
      * @param cardDom カードのDOM操作を行うインターフェース
@@ -20,15 +17,14 @@ var Card = /** @class */ (function () {
      * @param frontImageUrl 表面の画像のURL
      * @param onClickCallback カードがクリックされた際の処理
      */
-    function Card(cardDom, matchingKey, frontImageUrl, onClickCallback) {
-        var _this = this;
+    constructor(cardDom, matchingKey, frontImageUrl, onClickCallback) {
         this.flipStatus = FlipStatus.Back;
         this.matchingKey = matchingKey;
         this.frontImageUrl = frontImageUrl;
         this.onClickCallback = onClickCallback;
         this.cardDom = cardDom;
-        this.cardDom.onClick(function () {
-            _this.handleCardClick();
+        this.cardDom.onClick(() => {
+            this.handleCardClick();
         });
         this.flipCard(FlipStatus.Back);
     }
@@ -38,14 +34,14 @@ var Card = /** @class */ (function () {
      * @param frontImageUrl 新しい表面の画像のURL
      * @param onClickCallback 新しいカードがクリックされた際の処理
      */
-    Card.prototype.cloneWithNewImage = function (matchingKey, frontImageUrl, onClickCallback) {
+    cloneWithNewImage(matchingKey, frontImageUrl, onClickCallback) {
         return new Card(this.cardDom, matchingKey, frontImageUrl, onClickCallback);
-    };
+    }
     /**
      * カードの向きを指定された状態に変更します
      * @param flipStatus カードの新しい向き (表/裏)
      */
-    Card.prototype.flipCard = function (flipStatus) {
+    flipCard(flipStatus) {
         this.flipStatus = flipStatus;
         if (this.flipStatus === FlipStatus.Front) {
             this.cardDom.flipToFront(this.frontImageUrl);
@@ -53,31 +49,29 @@ var Card = /** @class */ (function () {
         else {
             this.cardDom.flipToBack();
         }
-    };
+    }
     /**
      * カードがクリックされた際の内部処理を定義します
      */
-    Card.prototype.handleCardClick = function () {
+    handleCardClick() {
         if (this.flipStatus === FlipStatus.Front) {
             return;
         }
         this.onClickCallback();
-    };
+    }
     /**
      * カードが今どちら向きを示しているかを返します
      */
-    Card.prototype.getFlipStatus = function () {
+    getFlipStatus() {
         return this.flipStatus;
-    };
+    }
     /**
      * 2枚のカードが一致するか判定します
      * @param card1 1つ目のカード
      * @param card2 2つ目のカード
      * @returns 2枚のカードが一致する場合はtrue、それ以外はfalse
      */
-    Card.canMatchCard = function (card1, card2) {
+    static canMatchCard(card1, card2) {
         return card1.matchingKey === card2.matchingKey;
-    };
-    return Card;
-}());
-exports.Card = Card;
+    }
+}
