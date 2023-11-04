272516aafb30d5b51aff4049c348baa73915c896
Bolero-fk <fuku_0000@outlook.jp>
2023-11-04 17:07:25 +0900

    validate関数を追加するためにconstructorを追加した

--- Scripts/gameConfig.js
+++ Scripts/gameConfig.js
@@ -2,4 +2,12 @@
  * ゲームの設定
  */
 export class GameConfig {
+    constructor(row, column, jsonPath, imageFolderPath, flippingWaitTimeMilliseconds, maxSelectableCard) {
+        this.row = row;
+        this.column = column;
+        this.jsonPath = jsonPath;
+        this.imageFolderPath = imageFolderPath;
+        this.flippingWaitTimeMilliseconds = flippingWaitTimeMilliseconds;
+        this.maxSelectableCard = maxSelectableCard;
+    }
 }
--- Scripts/gameConfig.ts
+++ Scripts/gameConfig.ts
@@ -8,4 +8,20 @@ export class GameConfig {
     public imageFolderPath: string;
     public flippingWaitTimeMilliseconds: number;
     public maxSelectableCard: number;
+
+    constructor(
+        row: number,
+        column: number,
+        jsonPath: string,
+        imageFolderPath: string,
+        flippingWaitTimeMilliseconds: number,
+        maxSelectableCard: number
+    ) {
+        this.row = row;
+        this.column = column;
+        this.jsonPath = jsonPath;
+        this.imageFolderPath = imageFolderPath;
+        this.flippingWaitTimeMilliseconds = flippingWaitTimeMilliseconds;
+        this.maxSelectableCard = maxSelectableCard;
+    }
 }
