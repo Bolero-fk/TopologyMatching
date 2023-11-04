21e1322287c96fb8b65f675f1c974897a0c80a92
Bolero-fk <fuku_0000@outlook.jp>
2023-11-04 18:02:34 +0900

    gameConfigクラスに入力された値の検証コードを追加した

--- Scripts/gameConfig.js
+++ Scripts/gameConfig.js
@@ -3,11 +3,35 @@
  */
 export class GameConfig {
     constructor(row, column, jsonPath, imageFolderPath, flippingWaitTimeMilliseconds, maxSelectableCard) {
+        this.Validate(row, column, jsonPath, imageFolderPath, flippingWaitTimeMilliseconds, maxSelectableCard);
         this.row = row;
         this.column = column;
         this.jsonPath = jsonPath;
         this.imageFolderPath = imageFolderPath;
         this.flippingWaitTimeMilliseconds = flippingWaitTimeMilliseconds;
         this.maxSelectableCard = maxSelectableCard;
+    }
+    Validate(row, column, jsonPath, imageFolderPath, flippingWaitTimeMilliseconds, maxSelectableCard) {
+        if (row <= 0) {
+            throw new Error('Row must be positive values.');
+        }
+        if (column <= 0) {
+            throw new Error('Column must be positive values.');
+        }
+        if (row * column % 2 !== 0) {
+            throw new Error('The product of row and column must be an even number.');
+        }
+        if (jsonPath.trim() === '') {
+            throw new Error('jsonPath must be a non-empty string');
+        }
+        if (imageFolderPath.trim() === '') {
+            throw new Error('imageFolderPath must be a non-empty string');
+        }
+        if (flippingWaitTimeMilliseconds <= 0) {
+            throw new Error('flippingWaitTimeMilliseconds must be a positive integer');
+        }
+        if (maxSelectableCard <= 0) {
+            throw new Error('maxSelectableCard must be a positive integer');
+        }
     }
 }
